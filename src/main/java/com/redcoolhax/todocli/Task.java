58505789c2bc92cccb02ef4e4d6df87eeab92866
Redcoolhax <>
2025-08-13 19:56:55 -0400

    Added documentation for the Task and TaskStatus classes.

--- src/main/java/com/redcoolhax/todocli/Task.java
+++ src/main/java/com/redcoolhax/todocli/Task.java
@@ -1,40 +1,73 @@
 package com.redcoolhax.todocli;
 
+/**
+ * Represents a Task within the to-do app. 
+ * Has data for a title, description, and status representing the current progress on said Task.
+ */
 public class Task {
-    private String name;
+    private String title;
     private String description;
     private TaskStatus status;
 
-    public Task(String name, String description, TaskStatus status) {
-        this.name = name;
+    /**
+     * Creates a new Task.
+     * @param title This Task's title.
+     * @param description This Task's description.
+     * @param status This Task's status representing its current progress.
+     */
+    public Task(String title, String description, TaskStatus status) {
+        this.title = title;
         this.description = description;
         this.status = status;
     }
 
-    public Task(String name, String description) {
-        this(name, description, TaskStatus.NOT_STARTED);
+    /**
+     * Creates a new Task whose status is the default value (TaskStatus.NOT_STARTED).
+     * @param title This Task's title.
+     * @param description This Task's description.
+     */
+    public Task(String title, String description) {
+        this(title, description, TaskStatus.NOT_STARTED);
     }
 
-    public String getName() {
-        return name;
+    /**
+     * @return This Task's title.
+     */
+    public String getTitle() {
+        return title;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * @param title This Task's title.
+     */
+    public void setTitle(String title) {
+        this.title = title;
     }
 
+    /**
+     * @return This Task's description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * @param description This Task's description.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * @return This Task's status representing its current progress.
+     */
     public TaskStatus getStatus() {
         return status;
     }
 
+    /**
+     * @param status This Task's status representing its current progress.
+     */
     public void setStatus(TaskStatus status) {
         this.status = status;
     }
--- src/main/java/com/redcoolhax/todocli/TaskStatus.java
+++ src/main/java/com/redcoolhax/todocli/TaskStatus.java
@@ -1,12 +1,31 @@
 package com.redcoolhax.todocli;
 
+/**
+ * Represents the current progress of a Task (see: com.redcoolhax.todocli.Task).
+ */
 public enum TaskStatus {
+    /**
+     * Status of a Task that hasn't been started.
+     */
     NOT_STARTED("Not started"),
+
+    /**
+     * Status of a Task that is currently in progress.
+     */
     IN_PROGESS("In progress"),
+
+    /**
+     * Status of a Task that has been finished.
+     */
     FINISHED("Finished");
 
     private String stringVer;
 
+    /**
+     * Creates a new TaskStatus, for which a String equivalent is given 
+     * (ie "Not started" for TaskStatus.NOT_STARTED).
+     * @param stringVer
+     */
     private TaskStatus(String stringVer) {
         this.stringVer = stringVer;
     }
